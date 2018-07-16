7b806045f19741490bee780712a9db48866d4236
cyccess <cyccess@gmail.com>
2018-07-16 18:15:11 +0800

    订单、库存、信用额度查询

---
+++ Goro.Check.Service/Model/StockViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goro.Check.Service.Model
+{
+    public class StockViewModel
+    {
+        public int Id { get; set; }
+        public string FItemNumber { get; set; }
+        public string FItemName { get; set; }
+        public string FItemModel { get; set; }
+        public string FStockName { get; set; }
+        public decimal FQty { get; set; }
+    }
+}
