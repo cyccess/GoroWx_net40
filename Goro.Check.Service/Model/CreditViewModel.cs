b36f9ec41d6f31c1bb22afd592a2184346098a64
cyccess <cyccess@gmail.com>
2018-07-16 18:15:03 +0800

    订单、库存、信用额度查询

---
+++ Goro.Check.Service/Model/CreditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goro.Check.Service.Model
+{
+    public class CreditViewModel
+    {
+        public int Id { get; set; }
+        public string FCustName { get; set; }
+        public decimal FAmount { get; set; }
+        public string FEmpName { get; set; }
+    }
+}
